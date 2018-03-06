92dd8621eb98f85c4b12b0560fbe7340b3194d35
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-05 21:26:42 -0500

    components added

--- logIn/src/app/auth/auth.module.ts
+++ logIn/src/app/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [EmailComponent, SignupComponent, MembersComponent]
 })
 export class AuthModule { }
