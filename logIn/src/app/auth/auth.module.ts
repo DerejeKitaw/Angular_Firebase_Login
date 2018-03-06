f2b472917d03b675471441ad34886bb05b4ac56a
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-05 21:28:01 -0500

    loging compoent added

--- logIn/src/app/auth/auth.module.ts
+++ logIn/src/app/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { EmailComponent } from './email/email.component';
 import { SignupComponent } from './signup/signup.component';
 import { MembersComponent } from './members/members.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [EmailComponent, SignupComponent, MembersComponent]
+  declarations: [EmailComponent, SignupComponent, MembersComponent, LoginComponent]
 })
 export class AuthModule { }
