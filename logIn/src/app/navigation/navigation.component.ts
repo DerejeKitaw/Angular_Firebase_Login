ac9e18ead987c08fd60a5bcd6a7c82e786daf96a
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-07 22:03:00 -0500

    navigation added

---
+++ logIn/src/app/navigation/navigation.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor(public authService: AuthService) { }
+
+  signOut() {
+    this.authService.logout();
+  }
+  ngOnInit() {
+  }
+
+}
