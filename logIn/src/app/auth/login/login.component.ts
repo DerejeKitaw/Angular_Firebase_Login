188a5412f51c39245a3ff877d6e0d51f03c0fb0d
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-07 22:04:05 -0500

    login Component added

--- logIn/src/app/auth/login/login.component.ts
+++ logIn/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  loginGoogle() {
+    this.authService.googleLogin();
   }
 
+  onSubmit(formData) {
+    if (formData.valid) {
+      this.authService.login(
+        formData.value.email,
+        formData.value.password
+      );
+    }
+  }
 }
