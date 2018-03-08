3a15d06624141eca413de7d936149ca79af1f10d
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-07 22:02:36 -0500

    router defined

--- logIn/src/app/app-routing.module.ts
+++ logIn/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DesignerComponent } from './designer/designer.component';
+import { AuthGuard } from './auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'pub/homepage', pathMatch: 'full' },
+  { path: 'pub/login', component: LoginComponent },
+  { path: 'pub/signup', component: SignupComponent },
+  { path: 'pub/homepage', component: HomePageComponent },
+  { path: 'designer', component: DesignerComponent , canActivate: [AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
